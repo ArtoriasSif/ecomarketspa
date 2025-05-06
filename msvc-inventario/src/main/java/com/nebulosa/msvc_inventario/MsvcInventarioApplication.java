666ffae7fce46007460455a3b270396627cc51f2
Matheus Lara <ma.delara@duocuc.cl>
2025-05-06 19:17:20 -0400

    primero comit

---
+++ msvc-inventario/src/main/java/com/nebulosa/msvc_inventario/MsvcInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.nebulosa.msvc_inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcInventarioApplication.class, args);
+	}
+
+}
