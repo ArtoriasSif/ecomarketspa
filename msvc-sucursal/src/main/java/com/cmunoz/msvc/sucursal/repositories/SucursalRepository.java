88388f34a250d488ac2a55ea1cb1f78ae44249da
Carlos-1984-Xe <carl.munozl@duocuc.cl>
2025-05-06 21:26:32 -0400

    micro de Sucursal

--- msvc-sucursal/src/main/java/com/cmunoz/msvc/sucursal/repositories/SucursalRepository.java
+++ msvc-sucursal/src/main/java/com/cmunoz/msvc/sucursal/repositories/SucursalRepository.java
@@ -2,9 +2,11 @@ package com.cmunoz.msvc.sucursal.repositories;
 
 import com.cmunoz.msvc.sucursal.models.Sucursal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
 
     Optional<Sucursal> findById(Long IdSucursal);
