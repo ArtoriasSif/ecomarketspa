4244df4269f20235a1b4763d74d8b3be0745268c
hanni <twitchxnukz@gmail.com>
2025-05-14 16:54:34 -0400

    Todo lo de feedback

---
+++ msvc-feedback/src/main/java/han/msvc_feedback/mscv_feedback/MscvFeedbackApplication.java
@@ -0,0 +1,13 @@
+package han.msvc_feedback.mscv_feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscvFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscvFeedbackApplication.class, args);
+	}
+
+}
