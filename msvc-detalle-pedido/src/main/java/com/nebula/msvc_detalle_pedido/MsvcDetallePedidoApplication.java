e2f8c83689923624be9e0633fe901e2b79746ad9
valparaiso <ma.delara@duocuc.cl>
2025-05-26 09:28:56 -0400

    Refactor

---
+++ msvc-detalle-pedido/src/main/java/com/nebula/msvc_detalle_pedido/MsvcDetallePedidoApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.msvc_detalle_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcDetallePedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcDetallePedidoApplication.class, args);
+	}
+
+}
---
+++ msvc-detalle-pedido/src/test/java/com/nebula/msvc_detalle_pedido/MsvcDetallePedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.msvc_detalle_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcDetallePedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
