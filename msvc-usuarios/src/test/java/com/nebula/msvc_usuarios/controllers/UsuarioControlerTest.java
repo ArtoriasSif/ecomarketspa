a79a69087ed86d3dba5f3d8be6c8d90c4eba2752
Matheus Lara <ma.delara@duocuc.cl>
2025-06-09 18:43:46 -0400

    inicio Test controler, Sucursal y Usuario

---
+++ msvc-usuarios/src/test/java/com/nebula/msvc_usuarios/controllers/UsuarioControlerTest.java
@@ -0,0 +1,7 @@
+package com.nebula.msvc_usuarios.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UsuarioControlerTest {
+}
