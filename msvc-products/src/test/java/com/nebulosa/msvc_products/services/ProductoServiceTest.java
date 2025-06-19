cc338025fb821d9dd4e5add0ea3003c82d86d9f7
Matheus Lara <ma.delara@duocuc.cl>
2025-06-18 21:11:18 -0400

    Test unitario de Producto finalzado

--- msvc-products/src/test/java/com/nebulosa/msvc_products/services/ProductoServiceTest.java
+++ msvc-products/src/test/java/com/nebulosa/msvc_products/services/ProductoServiceTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
