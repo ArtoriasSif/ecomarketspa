11ca1aaf72d0bb2a052f69e1fff53a1b85971747
Carlos-1984-Xe <carl.munozl@duocuc.cl>
2025-06-23 08:11:16 -0400

    Ultimo commit

---
+++ msvc-usuarios/src/main/java/com/nebula/msvc_usuarios/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.nebula.msvc_usuarios.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+                Contact contact = new Contact();
+                contact.setName("Carlos Muñoz");
+                contact.setEmail("carl.munozl@duocuc.cl");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - MSVC - Usuario")
+                        .version("1.0.0")
+                        .description("API de MSVC - Usuario, con el puedes realziar todas las consultas" +
+                                "CRUD que necesites")
+                        .contact(contact)
+                        .termsOfService("NO COLAPSAR LA API")
+                        .summary("Esta es una API de MSVC - Usuario")
+
+
+                );
+    }
+}
